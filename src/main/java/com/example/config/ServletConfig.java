06f52624e52ffa01912620afd97b1dee752e6bbf
dev-rashed-alam <dev.rashedalam@gmail.com>
2022-04-24 22:33:56 +0600

    Spring mvc basic configuration

---
+++ src/main/java/com/example/config/ServletConfig.java
@@ -0,0 +1,15 @@
+package com.example.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.example.controller"})
+public class ServletConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+}
