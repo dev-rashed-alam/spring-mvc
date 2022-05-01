8927563d56809fb2b4ae7fa1db5f166e781de6f2
dev-rashed-alam <dev.rashedalam@gmail.com>
2022-05-01 19:53:51 +0600

    use service annotation

--- src/main/java/com/example/config/RootConfig.java
+++ src/main/java/com/example/config/RootConfig.java
@@ -1,18 +1,17 @@
 package com.example.config;
 
 import com.example.service.Account;
-import com.example.service.AccountService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"com.example.service"})
 public class RootConfig {
     @Bean
-    public AccountService accountService(){
+    public Account account(){
         Account account = new Account();
         account.setAccountId("12345");
         account.setAccountHolder("Rashed Alam");
         account.setAccountType("Savings");
-        return new AccountService(account);
+        return account;
     }
 }
